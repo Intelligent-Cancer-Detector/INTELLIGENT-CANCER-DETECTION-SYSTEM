dc15a7fda4b4de1b2d21c74b5cf3a4f1e6892613
Isaac <mugwimiisaac230@gmail.com>
2026-05-04 13:23:05 +0300

    fix: update login links to point to the correct directory

--- backend/routes/__init__.py
+++ backend/routes/__init__.py
@@ -5,6 +5,7 @@ from .hospital2 import hospital_info_bp
 from .ml import ml_bp
 from .patient_history_routes import patient_assessment_history_bp
 from .assessment_routes import assessment_bp
+from .analytics_routes import analytics_bp
 
 # Blueprint = Router in Express
 
@@ -14,10 +15,13 @@ def register_routes(app):
     app.register_blueprint(auth_bp, url_prefix="/api/auth")
     app.register_blueprint(dashboard_bp, url_prefix="/api/dashboard")
     app.register_blueprint(
-        patient_assessment_history_bp, url_prefix="/api/patient-assessment/patient-history"
+        patient_assessment_history_bp,
+        url_prefix="/api/patient-assessment/patient-history",
     )
     app.register_blueprint(ml_bp, url_prefix="/api/ml")
 
     # hospital routes
     app.register_blueprint(hospital_info_bp, url_prefix="/api/hospital")
     app.register_blueprint(assessment_bp, url_prefix="/api/new-patient-assessment")
+
+    app.register_blueprint(analytics_bp,url_prefix="/api/analytics")
